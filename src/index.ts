8e0a25728d0f7b47b767697a06ac264c5fb0248a
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-04-03 20:47:53 +0700

    FCP-19 | fix app name

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ if (require.main === module) {
   // Run the application
   const config = {
     rest: {
-      port: +(process.env.PORT ?? 3000),
+      port: +(process.env.PORT ?? 3001),
       host: process.env.HOST,
       basePath: '/api',
       // The `gracePeriodForClose` provides a graceful close for http/https

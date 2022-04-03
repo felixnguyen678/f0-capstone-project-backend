d7f02fbc4358259953f1374f63e6a8fc35987016
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-04-03 17:15:05 +0700

    FCP-19 | create user.model

---
+++ src/models/user.model.ts
@@ -0,0 +1,62 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phoneNumber: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+  })
+  role?: string;
+
+  @property({
+    type: 'boolean',
+    default: true,
+  })
+  isEmailConfirmed?: boolean;
+
+  @property({
+    type: 'boolean',
+    default: true,
+  })
+  isActive?: boolean;
+
+  @property({
+    type: 'boolean',
+    default: false,
+  })
+  isDeleted?: boolean;
+
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;

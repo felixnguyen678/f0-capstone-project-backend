435a1cb165a01b5d18470d1c0962693cc7daaaeb
Giang Huynh <trgianghuynhdev1808@gmail.com>
2022-04-10 18:00:49 +0700

    FCP-27 | init doCloudService & implement authentication with DO

---
+++ src/constants/enums/index.ts
@@ -0,0 +1,3 @@
+export enum ERequestHeader {
+  DOAuthorization = 'do-authorization',
+}
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export * from './auth.service';
+export * from './do-cloud.service';
 export * from './hash.password.bcryptjs';
 export * from './jwt-spec.enhancer';
 export * from './jwt.service';
